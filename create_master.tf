255616d447b6ec4cac3de1c8a19cc96ade717b28
hugotms <45932591+hugotms@users.noreply.github.com>
2022-02-21 16:46:23 +0100

    feat: adding templates (#8)

--- create_master.tf
+++ create_master.tf
@@ -5,8 +5,8 @@ resource "vsphere_virtual_machine" "master" {
 
   num_cpus = 4
   num_cores_per_socket = 4
-  memory   = 16384
-  guest_id = "debian11_64Guest"
+  memory   = 8192
+  guest_id = "debian10_64Guest"
 
   network_interface {
     network_id = data.vsphere_network.network.id
@@ -23,11 +23,29 @@ resource "vsphere_virtual_machine" "master" {
     thin_provisioned = true
   }
 
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "${local.client_id}-master"
+        domain    = "geronimo.com"
+      }
+
+      network_interface {
+        ipv4_address = var.master_ip
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = var.gateway_ip
+    }
+  }
+
   provisioner "remote-exec" {
     script = "./bash/init.sh"
 
     connection {
-      host        = self.default_ip_address
+      host        = var.master_ip
       type        = "ssh"
       user        = "root"
       password    = var.root_password
