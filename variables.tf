0ea974895c1ec54fea61bb45c7def43622966d07
root <root@dev.labo.tisamo.loc>
2022-02-22 13:24:32 +0100

    fix

--- variables.tf
+++ variables.tf
@@ -75,6 +75,6 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "default_template"
+  name          = "ZUR-DEB-TEMPLATE"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
