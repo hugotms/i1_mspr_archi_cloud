5b06a6864a198489426d8c56fd5612cce3446a21
root <root@dev.labo.tisamo.loc>
2022-02-22 14:08:35 +0100

    fix

--- bash/init.sh
+++ bash/init.sh
@@ -7,7 +7,7 @@ mkdir -p /home/svcansible/.ssh
 chmod 700 /home/svcansible
 chmod 700 /home/svcansible/.ssh
 cat > /home/svcansible/.ssh/authorized_keys << EOF
-EOFecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACBod2GgHCeNO+EiXw8S7Ac9tRl1X3OYfZYqHd2VbFD0krvsu3AtCPmIViiHs9YFVUUdNPrYk5aUlpZpf1Azqb8pwDTeeGAfUlnIUZLxr57N0TzYIwfLz7OGQq5q4mldE9fL+4cYb8VGWjK4iEgDSaPFfl6S/AUFvMIm2nO1rreI7BGlg== root@dev.labo.tisamo.loc
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACBod2GgHCeNO+EiXw8S7Ac9tRl1X3OYfZYqHd2VbFD0krvsu3AtCPmIViiHs9YFVUUdNPrYk5aUlpZpf1Azqb8pwDTeeGAfUlnIUZLxr57N0TzYIwfLz7OGQq5q4mldE9fL+4cYb8VGWjK4iEgDSaPFfl6S/AUFvMIm2nO1rreI7BGlg== 
 EOF
 chmod 640 /home/svcansible/.ssh/authorized_keys
 chown -R svcansible:ansible /home/svcansible
